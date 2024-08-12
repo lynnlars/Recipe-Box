087345819c0f271ae7f32f9cd5bbe01d09e162f2
lynn larson <lynn.lars20@gmail.com>
2024-08-12 17:24:27 -0400

    added server code

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const {expressjwt} = require('express-jwt')
 require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const path = require('path')
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "dist")))
+
 app.use('/auth', require("./routes/authRouter"))
 app.use('/api/main', expressjwt({secret: process.env.SECRET, algorithms: ['HS256']}))
 app.use('/api/main/recipes', require('./routes/recipeRouter'))
@@ -27,6 +30,8 @@ app.use((err, req, res, next) => {
     console.log(err)
     return res.send({errMsg: err.message}) 
 })
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "client", "dist", "index.html")))
 
 //9Wll99ILAqzI57VU mongodb+srv://lynnlars20:9Wll99ILAqzI57VU@capstone.7h7tavn.mongodb.net/
 app.listen(9000, () =>{
